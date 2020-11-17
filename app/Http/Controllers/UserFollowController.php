e0d88b8a0be11efdfc436f84405fa718693a5886
hajime.saito <abc@abc.com>
2020-11-17 10:40:40 +0000

    follow, unfollow

---
+++ app/Http/Controllers/UserFollowController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserFollowController extends Controller
+{
+    // フォローアクション　User.phpのfollowメソッドを利用
+    public function store($id)
+    {
+        // 認証済みのユーザ（閲覧者）が、idのユーザをフォローする
+        \Auth::user()->follow($id);
+        
+        return back();
+    }
+    
+    // アンフォローのアクション
+    public function destroy($id)
+    {
+        // 認証済みのユーザがidのユーザをアンフォロー
+        \Auth::user()->unfollow($id);
+        
+        return back();
+    }
+}
--- app/User.php
+++ app/User.php
@@ -45,11 +45,68 @@ class User extends Authenticatable
         return $this->hasMany(Micropost::class);
     }
     
+    /**
+     * このユーザがフォロー中のユーザ。（ Userモデルとの関係を定義）
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+
+    /**
+     * このユーザをフォロー中のユーザ。（ Userモデルとの関係を定義）
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
      /**
      * このユーザに関係するモデルの件数をロードする。
      */
     public function loadRelationshipCounts()
     {
-        $this->loadCount('microposts');
+        $this->loadCount(['microposts', 'followings', 'followers']);
+    }
+    
+    public function follow($userID)
+    {
+        // 既にフォローしているかの確認
+        $exist = $this->is_following($userID);
+        // 相手が自分自身かどうかの確認
+        $its_me = $this->id == $userID;
+        
+        if($exist || $its_me) {
+            // すでにフォローしていれば何もしない
+            return false;
+        } else {
+            //未フォローであればフォロー
+            $this->followings()->attach($userID);
+            return true;
+        }
+    }
+    
+    public function unfollow($userId)
+    {
+        // すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 相手が自分自身かどうかの確認
+        $its_me = $this->id == $userId;
+        
+        // if($A && $B)とは$Aと$Bがともにtureならという意味
+        if ($exist && !$its_me) {
+            // すでにフォローしていればフォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            // 未フォローであれば何もしない
+            return false;
+        }
+    }
+        
+    public function is_following($userID) 
+    {
+        // フォロー中のユーザの中に$userIDのものが存在するか
+        return $this->followings()->where('follow_id', $userID)->exists();
     }
 }
